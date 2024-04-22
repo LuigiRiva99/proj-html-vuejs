d669b2e6721a6af7222f77cf0a8b5735168a9357
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-04-22 15:09:34 +0200

    ADD: button component, variables scss file, imported FontAwesome icons FIX: header links

---
+++ src/components/AppButton.vue
@@ -0,0 +1,33 @@
+<template>
+   <button>
+    {{ buttonText }}
+   </button>
+</template>
+
+<script>
+    export default {
+        props : {
+            buttonText: {
+                type: String,
+                default: 'ciao'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+button {
+    border: none;
+    padding: 20px 50px;
+    border-radius: 999px;
+    color: white;
+    background: rgb(139,224,100);
+    background: linear-gradient(90deg, rgba(139,224,100,1) 27%, rgba(0,217,166,1) 90%); 
+
+    &:hover{
+        cursor: pointer;
+        background: rgb(131,131,131);
+        background: linear-gradient(90deg, rgba(131,131,131,1) 0%, rgba(61,61,61,1) 47%, rgba(0,0,0,1) 79%); 
+    }
+}
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,9 @@
                 <p>(001) 88451234 88455438</p>
             </div>
             <div class="socials flex">
-                <a href="#">F</a>
-                <a href="#">X</a>
-                <a href="#">P</a>
+                <a href="#"><font-awesome-icon icon="fa-brands fa-facebook-f" /></a>
+                <a href="#"><font-awesome-icon icon="fa-brands fa-twitter" /></a>
+                <a href="#"><font-awesome-icon icon="fa-brands fa-pinterest" /></a>
             </div>
         </div>
         <div class="header_nav flex">
@@ -16,29 +16,59 @@
             </div>
             <ul class="link_list flex">
                 <li v-for="link in links">
-                    <a href="#">{{ link }}</a>
+                    <a :href="link.url">{{ link.text }}</a>
                 </li>
             </ul>
             <div class="search_bar">
-                <button>Purchase</button>
-                <i>L</i>
+                <AppButton class="header_button" :buttonText="buttonText"/>
+                <font-awesome-icon class="search_icon" icon="search"/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import AppButton from './AppButton.vue'
     export default {
+        components: {
+            AppButton
+        },
         data() {
             return {
-                links: ['HOME','ABOUT','SERVICES','SHOWCASE','BLOG','CONTACT']
+                links: [
+                    {
+                        text: 'HOME',
+                        url: '#'
+                    },
+                    {
+                        text: 'ABOUT',
+                        url: '#'
+                    },
+                    {
+                        text: 'SERVICES',
+                        url: '#'
+                    },
+                    {
+                        text: 'SHOWCASE',
+                        url: '#'
+                    },
+                    {
+                        text: 'BLOG',
+                        url: '#'
+                    },
+                    {
+                        text: 'CONTACT',
+                        url: '#'
+                    },
+                ],
+                buttonText: 'PURCHASE'
             }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
+@use '../style/partials/variables' as *;
 //header contatti 
 .header_contacts {
     height: 40px;
@@ -57,9 +87,18 @@
     padding: 10px 20px;
 }
 
+.socials {
+    gap: 15px;
+}
+
 //header nav
 
 .link_list {
-    gap: 15px;
+    gap: 50px;
+}
+
+.search_icon {
+    color: $brand-color;
+    margin-left: 20px;
 }
 </style>
